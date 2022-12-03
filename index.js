cd98abf2ce32c84223a7b0924a4e3c395eec07d9
Kenhubhub <98609221+Kenhubhub@users.noreply.github.com>
2022-12-03 21:48:50 +0000

    square objects now tore their corresponding neighbours

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
 const grid = document.getElementById("grid");
+// Obtain objects with square class 
+// Arithmetic to obtain coordinates for current square
+// compare each square with target coordinates and xc and yc
 
 const sizeSquare = 50;
-const Squares = []
+let Squares = []
+const compareCoord = (coord1, coord2)=>{
+    // console.log( coord1.x === coord2.x && coord1.y === coord2.y)
+    return String(coord1.x) ==coord2.x && String(coord1.y) == coord2.y;
+
+}
+const setNeighbours = (squares,squareElements)=>{
+    
+    squares.forEach(square => {
+        const x = square.x;
+        const y = square.y;
+        const left = {x:x-1,y:y};
+        const right = {x:x+1,y:y};
+        const up = {x:x , y: y-1};
+        const down = {x:x ,y: y+1};
+        const tl = {x:x-1,y:y-1};
+        const bl = {x:x-1,y:y+1};
+        const tr ={x: x+1, y:y-1};
+        const br = {x: x+1, y:y+1};
+        squareElements.forEach( element =>{
+            const xc = element.getAttribute("xc");
+            const yc = element.getAttribute("yc");
+            const elementcoord = {x:xc,y:yc};
+            compareCoord(left,elementcoord) ? square.neighbours.push(element) : false;
+            compareCoord(right,elementcoord) ? square.neighbours.push(element) : false; 
+            compareCoord(up,elementcoord) ? square.neighbours.push(element) : false; 
+            compareCoord(down,elementcoord) ? square.neighbours.push(element) : false; 
+            compareCoord(tl,elementcoord) ? square.neighbours.push(element) : false; 
+            compareCoord(bl,elementcoord) ? square.neighbours.push(element) : false; 
+            compareCoord(tr,elementcoord) ? square.neighbours.push(element) : false; 
+            compareCoord(br,elementcoord) ? square.neighbours.push(element) : false; 
+        })
+    
+    })
+}
 grid.addEventListener("click",e=>{
      e.target.style.backgroundColor = "red";
+     setNeighbours(Squares,Array.from(document.getElementsByClassName("square")));
+     Squares.forEach(square =>{
+        console.log("here be neighbours");
+        console.log(square.neighbours)
+     })
 })
 
 class Square{
@@ -22,14 +64,12 @@ class Square{
         this.square = square;
         grid.append(square);
     }
-    getNeighbourCoordinates(){
-        
-    }
 }
 class Grid{
     constructor(height,width){
         this.height = height;
         this.width = width;
+        this.squares = [];
     }
     gridInit = ()=>{
         const number = (this.height * this.width);
@@ -39,53 +79,27 @@ class Grid{
        
         for(let i = 0 ; i < number; i++){
             const newSquare = new Square();
-            Squares.push(newSquare);
+            this.squares.push(newSquare);
         }
         let x= 0;
         let y = 0;
-        Squares.forEach(square =>{
+        this.squares.forEach(square =>{
             square.x = x;
             square.y = y;
             square.square.setAttribute("xc",x)
             square.square.setAttribute("yc",y)
-            x=== width-1 ? (x = 0,y++) : x++;
+            x=== this.width-1 ? (x = 0,y++) : x++;
         })
     }
 }
 
-// const gridInit = ()=>{
-//     const number = (height * width);
-//     console.log(number)
-//     grid.style.height = `${height * 50}px`;
-//     grid.style.width = `${width * 50}px`;
-//     grid.style.gridTemplateColumns = `repeat(${width},1fr)`
-   
-//     for(let i = 0 ; i < number; i++){
-//         const newSquare = new Square();
-//         Squares.push(newSquare);
-//     }
-//     let x= 0;
-//     let y = 0;
-//     Squares.forEach(square =>{
-//         square.x = x;
-//         square.y = y;
-//         square.square.setAttribute("xc",x)
-//         square.square.setAttribute("yc",y)
-//         x=== width-1 ? (x = 0,y++) : x++;
-//     })
-//     //find neighbours
-//     for(let i = 0 ; i<Squares.length; i++){
-//         //get current squares
-//         getNeighbours(Squares[i])
-//     }
-// }
 
-// gridInit();
 const main = ()=>{
     const height = Number(prompt("Enter height"));
     const width = Number(prompt("Enter width"));
     const newGrid = new Grid(height,width);
     newGrid.gridInit();
+    Squares = newGrid.squares;
 }
 
 main();
